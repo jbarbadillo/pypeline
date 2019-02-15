68d30f85a7b99b999e4e9f865ed2fd0153330f5e
jbarbadillo <javier.barbadillo@gmail.com>
2019-02-15 17:20:33 +0100

    Implement either

--- pypeline/pypeline.py
+++ pypeline/pypeline.py
@@ -1,3 +1,14 @@
 # -*- coding: utf-8 -*-
 
-"""Main module."""
+"""Pypeline implements railway oriented programming methods to 
+   build a pipeline of functions with error handling"""
+
+class Either:
+    """
+    Implementation of Either monad, from Haskell.
+    """
+    def is_success(self):
+        pass
+    
+    def is_failure(self):
+        pass
