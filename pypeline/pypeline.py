c108d13a14f768c656eefa69ff5f1e327bb5bb48
jbarbadillo <javier.barbadillo@gmail.com>
2019-02-15 17:28:18 +0100

    Implement failure and success paths

--- pypeline/pypeline.py
+++ pypeline/pypeline.py
@@ -7,8 +7,22 @@ class Either:
     """
     Implementation of Either monad, from Haskell.
     """
+    def __init__(self, response):
+        self.response = response
+
     def is_success(self):
         pass
     
     def is_failure(self):
         pass
+
+class Success(Either):
+    """Success either path"""
+    def is_failure(self):
+        return False
+
+class Failure(Either):
+    """Failure either path"""
+
+    def is_failure(self):
+        return True
